845d57128ee992dfa9acae32e7e9f9008e6f83e0
yy yu <harrykang@zixuans-mac.local>
2018-11-28 09:31:17 -0500

    Initial Commit

---
+++ SoundBoard App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard App
+//
+//  Created by Zixuan Kang on 11/28/18.
+//  Copyright © 2018 Zixuan Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
